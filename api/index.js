8de51564f862a209936719f47942834f17f39908
Martin Vielvoye <martin.vielvoye@gmail.com>
2024-05-02 11:06:03 +0200

    data test

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const data = require("../data.json")
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/data", (req, res) => {
+  console.log({data});
+  res.send(data)
+})
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
